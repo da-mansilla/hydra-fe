aa67c168e6f13a3119f7f0c1d73839071760212f
Agustin <mansillaagustin1019@gmail.com>
2024-09-07 17:53:07 -0300

    Add list expedientes

--- src/app/expedientes/features/expediente-list/expediente-list.component.ts
+++ src/app/expedientes/features/expediente-list/expediente-list.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { HeadingTitleComponent } from '../../../shared/ui/heading-title/heading-title.component';
+import { InputSearchComponent } from '../../../shared/ui/input-search/input-search.component';
 
 @Component({
   selector: 'app-expediente-list',
   standalone: true,
-  imports: [HeadingTitleComponent],
+  imports: [HeadingTitleComponent,InputSearchComponent],
   templateUrl: './expediente-list.component.html',
-  styleUrl: './expediente-list.component.scss'
 })
 export default class ExpedienteListComponent {
+  ESTADOS = ["Pendiente","En Evaluacion","Aprobado","Rechazado"];
+  expedientes = [
+    {id:1, nombre:"Juan Perez", estado:"Pendiente" , "numeroExpediente":"A-1234"},
+    {id:2, nombre:"Hernan Sosa", estado:"En Evaluacion", "numeroExpediente":"A-1235"},
+    {id:3, nombre:"Damian Llosa", estado:"Aprobado", "numeroExpediente":"A-1236"},
+    {id:4, nombre:"Ricardo Nieves", estado:"Rechazado", "numeroExpediente":"A-1237"},
+  ];
 
 }
