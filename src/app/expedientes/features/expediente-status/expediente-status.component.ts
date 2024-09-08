ece943806071b5c90339528f54965950efb84ec5
Agustin <mansillaagustin1019@gmail.com>
2024-09-08 20:06:49 -0300

    Terminate Status page

--- src/app/expedientes/features/expediente-status/expediente-status.component.ts
+++ src/app/expedientes/features/expediente-status/expediente-status.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, input, Input } from '@angular/core';
+import { HeadingTitleComponent } from '../../../shared/ui/heading-title/heading-title.component';
 
 @Component({
   selector: 'app-expediente-status',
   standalone: true,
-  imports: [],
+  imports: [HeadingTitleComponent],
   templateUrl: './expediente-status.component.html',
   styles: ``
 })
 export default class ExpedienteStatusComponent {
-
+  id = input.required<string>();
+  expediente = {
+      id : this.id,
+      nombre : "Agustin Mansilla",
+      dni : "12345678",
+      numeroExpediente: "A-1234",
+      estado : "Activo",
+  }
 }
