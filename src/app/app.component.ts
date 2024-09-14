84f923f9f491f1ce277315400bf1cf1f63c2d1a8
Agustin <mansillaagustin1019@gmail.com>
2024-09-14 09:49:20 -0300

    Hide sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { SidebarComponent } from './shared/ui/sidebar/sidebar.component';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,5 +11,19 @@ import { SidebarComponent } from './shared/ui/sidebar/sidebar.component';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  constructor(private router:Router){
+
+  }
+  ngOnInit() {
+
+    this.router.events.pipe(filter(event => event instanceof NavigationEnd))
+    .subscribe((event: NavigationEnd) => {
+      this.showSidebar = event.url !== '/login';
+    });
+
+  }
   title = 'hydra-fe';
+  showSidebar = true;
+
+
 }
