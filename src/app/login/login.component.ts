9ef0e1a71ed3220bf805adc1971aa4f9adb3063c
Agustin <mansillaagustin1019@gmail.com>
2024-09-14 10:07:08 -0300

    Fix

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styles: ``
 })
 export default class LoginComponent {
+  constructor(private router: Router){
+
+  }
+  mail = '';
+  password = '';
+
+  onLogin(event: Event) {
+    event.preventDefault();
+    if (this.mail === 'admin' && this.password === 'admin') {
+      console.log('Login successful');
+      
+      this.router.navigate(['/home']);
+    }else{
+      console.log('Login failed');
+      console.log('Mail: ', this.mail);
+      console.log('Password: ', this.password);
+    }
+  }
 
 }
